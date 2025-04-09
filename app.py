0625e4aefe4094334aa86a872595effe57b0d170
Nipunkhattri <nipunkhattri321@gmail.com>
2025-04-09 06:50:31 -0700

    allowed from all origin

--- app.py
+++ app.py
@@ -13,13 +13,9 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
-origins = [
-    "https://nipunkhattri-shl-assessment-recommendor-streamlit-bfh1ad.streamlit.app/"
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
